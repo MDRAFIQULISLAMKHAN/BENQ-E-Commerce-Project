dd016d544acd22576060333819e5e8bad39181e3
Hillol Sarker <hillolsarker9@outlook.com>
2021-08-19 11:24:06 +0600

    Common utility

--- Bh.DataLayerSql/CommonUtility.cs
+++ Bh.DataLayerSql/CommonUtility.cs
@@ -20,87 +20,82 @@ namespace BH.DataLayerSql
 
         // Company Details
         public static string GetCompanyDetails = "sp_GetCompanyDetailbyID";
-     
 
-
-        /*// Banner
+        //Banner
+        public static string GetAllBanner = "sp_GetBanner";
         public static string GetBannerById = "sp_GetBannerDetails";
         public static string InsertBanner = "sp_InsertBanner";
         public static string UpdateBanner = "sp_UpdateBanner";
-        public static string GetAllBanners = "sp_GetBanner";
         public static string DeleteBanner = "sp_DeleteBanner";
+        
+        //Brand
+        public static string GetAllBrand = "sp_GetBrand";
+        public static string GetBrandById = "sp_GetBrandDetails";
+        public static string InsertBrand = "sp_InsertBrand";
+        public static string UpdateBrand = "sp_UpdateBrand";
+        public static string DeleteBrand = "sp_DeleteBrand";
+        
+        //Manufactureres
+        public static string GetAllManufacturers = "sp_GetManufacturers";
+        public static string GetManufacturersById = "sp_GetManufacturerDetails";
+        public static string InsertManufacturers = "sp_InsertManufacturer";
+        public static string UpdateManufacturers = "sp_UpdateManufacturer";
+        public static string DeleteManufacturers = "sp_DeleteManufacturer";
+        
+        //Product Categories
+        public static string GetAllProductCategories = "sp_GetProductCategories";
+        public static string GetProductCategoriesById = "sp_GetProductCategorieDetails";
+        public static string InsertProductCategories = "sp_InsertProductCategorie";
+        public static string UpdateProductCategories = "sp_UpdateProductCategorie";
+        public static string DeleteProductCategories = "sp_DeleteProductCategorie";
+        
+        
+        //Product Gallery
+        public static string GetAllProductGallery = "sp_GetProductGallery";
+        public static string GetProductGalleryById = "sp_GetProductGalleryDetails";
+        public static string InsertProductGallery = "sp_InsertProductGallery";
+        public static string UpdateProductGallery = "sp_UpdateProductGallery";
+        public static string DeleteProductGallery = "sp_DeleteProductGallery";
+        
+        
+        //Product
+        public static string GetAllProduct = "sp_GetProduct";
+        public static string GetProductById = "sp_GetProductDetails";
+        public static string InsertProduct = "sp_InsertProduct";
+        public static string UpdateProduct = "sp_UpdateProduct";
+        public static string DeleteProduct = "sp_DeleteProduct";
+        
+        
+        //Product Sub Categories
+        public static string GetAllProductSubCategories = "sp_GetProductSubCategories";
+        public static string GetProductSubCategoriesById = "sp_GetProductSubCategorieDetails";
+        public static string InsertProjectSubCategory = "sp_InsertProductSubCategorie";
+        public static string UpdateProjectSubCategory = "sp_UpdateProductSubCategorie";
+        public static string DeleteProjectSubCategory = "sp_DeleteProductSubCategorie";
+        
+        
+        //Product Type
+        public static string GetAllProductType = "sp_GetProductType";
+        public static string GetProductTypeById = "sp_GetProductTypeDetails";
+        public static string InsertProductType = "sp_InsertProductType";
+        public static string UpdateProductType = "sp_UpdateProductType";
+        public static string DeleteProductType = "sp_DeleteProductType";
+        
+        
+        //Sale
+        public static string GetAllSale = "sp_GetSale";
+        public static string GetSaleById = "sp_GetSaleDetails";
+        public static string InsertSale = "sp_InsertSale";
+        public static string UpdateSale = "sp_UpdateSale";
+        public static string DeleteSale = "sp_DeleteSale";
+        
+        
+        //Tax Category
+        public static string GetAllTaxCategory = "sp_GetTaxCategory";
+        public static string GetTaxCategoryById = "sp_GetTaxCategoryDetails";
+        public static string InsertTaxCategory = "sp_InsertTaxCategory";
+        public static string UpdateTaxCategory = "sp_UpdateTaxCategory";
+        public static string DeleteTaxCategory = "sp_DeleteTaxCategory";
 
-        // About Us
-        public static string GetAboutDetails = "sp_GetAboutUDetails";
-        public static string UpdateAbout = "sp_UpdateAboutU";
-
-
-        // Our Clients Banner
-        public static string GetClientBannersDetails = "sp_GetClientBannersDetails";
-        public static string UpdateClientsBanner = "sp_UpdateClientsBanner";
-
-
-        // Clients
-        public static string GetAllClients = "sp_GetOurClient";
-        public static string GetClientById = "sp_GetOurClientDetails";
-        public static string InsertClient = "sp_InsertOurClient";
-        public static string UpdateClient = "sp_UpdateOurClient";
-        public static string DeleteClient = "sp_DeleteOurClient";
-
-        // Company Setting
-        public static string GetCompanySetting = "sp_GetCompanySettingDetails";
-        public static string UpdateCompanySettings = "sp_UpdateCompanySetting";
-
-        // Machines
-        public static string GetAllMachines = "sp_GetOurMachine";
-        public static string GetMachineById = "sp_GetOurMachineDetails";
-        public static string InsertMachine = "sp_InsertOurMachine";
-        public static string UpdateMachine = "sp_UpdateOurMachine";
-        public static string DeleteMachine = "sp_DeleteOurMachine";
-
-        // Services
-        public static string GetAllServices = "sp_GetOurService";
-        public static string GetServicesById = "sp_GetOurServiceDetails";
-        public static string InsertServices = "sp_InsertOurService";
-        public static string UpdateServices = "sp_UpdateOurService";
-        public static string DeleteService = "sp_DeleteOurService";
-
-        //Projects
-        public static string GetAllProjects = "sp_GetProject";
-        public static string GetProjectsById = "sp_GetProjectDetails";
-        public static string InsertProjects = "sp_InsertProject";
-        public static string UpdateProjects = "sp_UpdateProject";
-        public static string DeleteProject = "sp_DeleteProject";
-
-        // Project Category
-        public static string GetAllProjectCategory = "sp_GetProjectCategory";
-        public static string GetProjectCategoryById = "sp_GetProjectCategoryDetails";
-        public static string InsertProjectCategory = "sp_InsertProjectCategory";
-        public static string UpdateProjectCategory = "sp_UpdateProjectCategory";
-        public static string DeleteProjectCategory = "sp_DeleteProjectCategory";
-
-        // Project SubCategory
-        public static string GetAllProjectSubCategory = "sp_GetProjectSubCategory";
-        public static string GetProjectSubCategoryById = "sp_GetProjectSubCategoryDetails";
-        public static string InsertProjectSubCategory = "sp_InsertProjectSubCategory";
-        public static string UpdateProjectSubCategory = "sp_UpdateProjectSubCategory";
-        public static string DeleteProjectSubCategory = "sp_DeleteProjectSubCategory";
-        public static string GetSubCategoryByCategoryId = "sp_GetSubCategoryByCategoryId";
-
-        // Project Gallery
-        public static string GetAllProjectGallery = "sp_GetProjectGallery";
-        public static string GetProjectGalleryById = "sp_GetProjectGalleryDetails";
-        public static string InsertProjectGallery = "sp_InsertProjectGallery";
-        public static string UpdateProjectGallery = "sp_UpdateProjectGallery";
-        public static string DeleteProjectGallery = "sp_DeleteProjectGallery";
-        public static string GetAllProjectsByCategoryId = "sp_GetProjectsByCategoryId";
-        public static string GetAllProjectsBySubCategoryId = "sp_GetProjectsBySubCategoryId";
-        public static string GetAllProjectGalleriesByProjectId = "sp_GetProjectGalleriesByProjectId";
-        // Feedback
-        public static string GetAllFeedback = "sp_GetFeedback";
-        public static string GetFeedbackById = "sp_GetFeedbackDetails";
-        public static string InsertFeedback = "sp_InsertFeedback";
-        public static string UpdateFeedback = "sp_UpdateFeedback";
-        public static string DeleteFeedback = "sp_DeleteFeedback";*/
     }
 }
