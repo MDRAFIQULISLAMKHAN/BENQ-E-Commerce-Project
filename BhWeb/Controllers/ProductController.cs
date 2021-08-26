30e24be76ed0b38ea682e42e1d2985587cf3a44e
Md Rafiqul Islam Khan <mdrafiqulislamkhan@hotmail.com>
2021-08-26 13:41:39 +0600

    asd

--- BhWeb/Controllers/ProductController.cs
+++ BhWeb/Controllers/ProductController.cs
@@ -24,6 +24,7 @@ namespace BhWeb.Controllers
             publicViewModel.Product = result.First();
             publicViewModel.ProductGallery = ProductManager.GetProductGallery(publicViewModel.Product.ProductID);
             return View("~/Views/Product/Benq_Product.cshtml", publicViewModel);
+
         }
     }
 }
