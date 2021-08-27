a3df39cad73904eaaa64ab0cffc990dbe1ba915a
Hillol Sarker <hillolsarker9@outlook.com>
2021-08-27 17:17:39 +0600

    latest deals of the day

--- Bh.Models/PublicViewModel.cs
+++ Bh.Models/PublicViewModel.cs
@@ -16,8 +16,9 @@ namespace BH.Models
         public List<ProductModel> TrendingList { get; set; }
         public List<StoreModel> StoreList { get; set; }
         public List<DealModel> DealList { get; set; }
-        public DealModel Deal1 { get; set; }
-        public DealModel Deal2 { get; set; }
+        public DealModel Deal { get; set; }
+        public List<DealModel> dealsofTheday { get; set; }
+        public List<ProductModel> DealsofthedayProductList { get; set; }
 
         public ProductModel Product { get; set; }
         public List<ProductGalleryModel> ProductGallery { get; set; }
