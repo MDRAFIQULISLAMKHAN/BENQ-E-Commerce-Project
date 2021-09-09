2dc9aac5b0373b5af6bd6a1d31ec356645f16f9d
Md Rafiqul Islam Khan <mdrafiqulislamkhan@hotmail.com>
2021-09-09 17:07:25 +0600

    SDasd

--- BhWeb/Controllers/CartController.cs
+++ BhWeb/Controllers/CartController.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -19,35 +23,57 @@ namespace BhWeb.Controllers
             return View("",av);
         }
 
-        /*[HttpPost]
-        [ValidateInput(false)]
-        public ActionResult InsertCart(PublicViewModel av)
-        {
-            if (av.AddtoCart != null && av.AddtoCart.CartID > 0)
-            {
 
-                av.AddtoCart.CreatedDate = DateTime.Today;
-                av.AddtoCart.CreatedBy = "Admin";
-                CartManager.UpdateCart(av.AddtoCart);
+        [HttpPost]
+        public JsonResult AddToCart(CartModel cartItem)
+        {
+            var result = AddDetails(cartItem);
+            return Json(new { Success = true, Message = result }, JsonRequestBehavior.AllowGet);
+        }
+
+        /*[HttpPost]
+        public JsonResult Index(ItemViewModel objItemViewModel)
+        {
+            string NewImage = Guid.NewGuid() + Path.GetExtension(objItemViewModel.ImagePath.FileName);
+            objItemViewModel.ImagePath.SaveAs(Server.MapPath("~/Images/" + NewImage));
+
+            Item objItem = new Item();
+            objItem.ImagePath = "~/Images/" + NewImage;
+            objItem.CategoryId = objItemViewModel.CategoryId;
+            objItem.Description = objItemViewModel.Description;
+            objItem.ItemCode = objItemViewModel.ItemCode;
+            objItem.ItemId = Guid.NewGuid();
+            objItem.ItemName = objItemViewModel.ItemName;
+            objItem.ItemPrice = objItemViewModel.ItemPrice;
+            objECartDbEntities.Items.Add(objItem);
+            objECartDbEntities.SaveChanges();
+
+            return Json(new { Success = true, Message = "Item is added Successfully." }, JsonRequestBehavior.AllowGet);
+        }*/
+        //To add Records into database     
+        private string AddDetails(CartModel cartItem)
+        {
+            CartManager newCart = new CartManager();
+            if (cartItem != null && cartItem.CartID > 0)
+            {
+                cartItem.CreatedDate = DateTime.Today;
+                cartItem.CreatedBy = "Admin";
+                CartManager.UpdateCart(cartItem);
+                return "Cart Updated Successfully";
             }
             else
             {
-                if (av.AddtoCart != null)
+                if (cartItem != null)
                 {
-                   
-                        av.ProjectCategory.ImageUrl = _UploadSingleImage(av, image);
-                        av.ProjectCategory.IsActive = true;
-                        av.ProjectCategory.CreatedDate = DateTime.Today;
-                        av.ProjectCategory.CreatedBy = "Admin";
-                        CategoryManager.InsertProjectCategory(av.ProjectCategory);
-                  
-                        FlashMessage.Danger("Image Required");
-                        return View(av);
-                   
+                        cartItem.CreatedDate = DateTime.Today;
+                        cartItem.CreatedBy = "Admin";
+                        CartManager.InsertCart(cartItem);
+                        return "Added to Cart Successfully";
                 }
             }
 
-            return RedirectToAction("Categories");
-        }*/
+            return "Added to Cart Faild";
+        }
+
     }
 }
